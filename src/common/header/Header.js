743c1d82b237ec2971c6d7834e3f7afd32c111cd
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-09-23 20:35:00 +0530

    Added Tabs inside modal for Login button

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,4 +1,4 @@
-import { Button } from '@material-ui/core';
+import { Button, Tabs, Tab } from '@material-ui/core';
 import React, { Component, Fragment } from 'react';
 import '../header/Header.css';
 import logo from '../../assets/logo.svg';
@@ -9,7 +9,8 @@ class Header extends Component {
     constructor() {
         super();
         this.state = {
-            modalIsOpen: false
+            modalIsOpen: false,
+            value: 0
         }
     }
 
@@ -25,6 +26,12 @@ class Header extends Component {
         })
     }
 
+    tabChangeHandler = (event, value) => {
+        this.setState({
+            value: value
+        })
+    }
+
     render() {
         return (
             <Fragment>
@@ -34,10 +41,15 @@ class Header extends Component {
                         <Button onClick={this.openModalHandler} variant="contained" color="default">Login</Button>
                     </div>
                 </header>
-                <Modal ariaHideApp={false} 
-                isOpen={this.state.modalIsOpen} 
-                contentLabel="Login"
-                onRequestClose={this.closeModalHandler}></Modal>
+                <Modal ariaHideApp={false}
+                    isOpen={this.state.modalIsOpen}
+                    contentLabel="Login"
+                    onRequestClose={this.closeModalHandler}>
+                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                        <Tab label="Login" />
+                        <Tab label="Register" />
+                    </Tabs>
+                </Modal>
             </Fragment>
         )
     }
