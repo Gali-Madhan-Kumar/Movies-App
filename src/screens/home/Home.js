5f9885619cd60e114dde6af0fcbf6aa667393935
Gali-Madhan-Kumar <gmadhan99@gmail.com>
2020-09-23 16:05:43 +0530

    Added ‘Home’ component

---
+++ src/screens/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../home/Home.css';
+
+class Home extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Home;
